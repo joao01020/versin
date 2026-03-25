c58f515c4a7e19be85f2cacd0cd3fd394161ee23
joao01020 <joaosaless26@gmail.com>
2026-03-25 15:49:11 -0300

    config: ajuste de inicialização, deep linking e dependências
    - Configura o filtro de intent no AndroidManifest.
    - Ajusta ordem de inicialização do Supabase no main.dart.
    - Atualiza dependências no pubspec.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import necessário para o .env
 import 'package:versin/features/rimas/presentation/pages/chat_page.dart';
 
-void main() {
-  // Garante que os widgets do Flutter estejam inicializados antes de rodar o app
+void main() async {
+  // 1. Garante que os widgets do Flutter estejam inicializados antes de rodar o app
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Carrega as variáveis de ambiente do arquivo .env (Raiz do projeto)
+  await dotenv.load(fileName: ".env");
+
+  // 3. Inicializa o Supabase do Versin Genesis usando o dotenv
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
+
+  // linha temporariamente para deslogar sessão lixo no cache (remover depois dos testes)
+  // Agora está DEPOIS do initialize, o que evita o erro de Assertion
+  await Supabase.instance.client.auth.signOut();
+
   runApp(const MyApp());
 }
 
